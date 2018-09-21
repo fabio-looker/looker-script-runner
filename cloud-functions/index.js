07577173c4664cbacbddb3beba7adbc9bda7644e
fabio-looker <fabio@looker.com>
2018-09-21 19:17:32 -0400

    reorg folders

---
+++ cloud-functions/index.js
@@ -0,0 +1,3 @@
+exports.cors = require('./cors')
+exports.authCheckOnline = require('./online-auth-check')
+exports.static = require('./static')
--- stateless-admin-server/index.js
+++
@@ -1,2 +0,0 @@
-exports.cors = require('./cors')
-exports.onlineAuthCheck = require('./online-auth-check')
